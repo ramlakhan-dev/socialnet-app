5f02bb06dd6e59f060caefb49f2dfa6b093abff6
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2025-09-25 20:50:00 +0530

    setup hilt for dependency injection

--- app/src/main/java/com/rl/socialnet/MainActivity.kt
+++ app/src/main/java/com/rl/socialnet/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.rl.socialnet.presentation.ui.theme.SocialNetTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
